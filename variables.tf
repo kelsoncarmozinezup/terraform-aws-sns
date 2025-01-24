237dbedd9d93b38654020d8c1660cbef79266476
kelsoncarmizinezup <kelson.carmozine@zup.com.br>
2025-01-24 14:08:24 -0300

    fix: variable

--- variables.tf
+++ variables.tf
@@ -2,20 +2,3 @@ variable "name" {
   description = "The name of the SNS topic"
   type        = string
 }
-
-variable "environment" {
-  type = map(string)
-  default = {
-    "185072717495" = "dev"
-    "300543077521" = "stg"
-    "742799255367" = "prd"
-  }
-}
-
-variable "inputs" {
-    description = "Inputs for STK"
-    type = object({
-      topic_name = string
-      workspace  = string
-    })
-}
